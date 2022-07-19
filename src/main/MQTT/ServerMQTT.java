cb4cda13d4c0a1444667cf2c8b2b9f5131d33c3e
billy <83074959+billyqu@users.noreply.github.com>
2022-07-19 02:03:42 -0400

    can receive message

---
+++ src/main/MQTT/ServerMQTT.java
@@ -0,0 +1,57 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+public class ServerMQTT {
+    public static final String HOST = "";
+    public static final String TOPIC = "";
+    private static final String clientid = "server11";
+    private MqttClient client;
+    private MqttTopic topic11;
+    private String userName = "mosquitto";
+    private String passWord = "mosquitto";
+    private MqttMessage message;
+
+    public ServerMQTT() throws MqttException{
+        client = new MqttClient(HOST, clientid, new MemoryPersistence());
+        connect();
+    }
+
+    public void connect() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(false);
+        options.setUserName(userName);
+        options.setPassword(passWord.toCharArray());
+        options.setConnectionTimeout(10);
+        options.setKeepAliveInterval(20);
+        try {
+            client.setCallback(new PushCallBack());
+            client.connect(options);
+            topic11 = client.getTopic(TOPIC);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(MqttTopic topic, MqttMessage message) throws MqttPersistenceException, MqttException{
+        MqttDeliveryToken token = topic.publish(message);
+        token.waitForCompletion();
+        System.out.println("message is published completely! " + token.isComplete());
+    }
+
+    public static void main(String[] args) throws MqttException{
+        ServerMQTT server = new ServerMQTT();
+        server.message = new MqttMessage();
+        server.message.setQos(1);
+        server.message.setRetained(true);
+        server.message.setPayload("hello,topic14".getBytes());
+        server.publish(server.topic11, server.message);
+        System.out.println(server.message.isRetained() + "------ratained状态");
+    }
+}
